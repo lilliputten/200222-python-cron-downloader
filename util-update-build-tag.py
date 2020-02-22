fadfeeefd83eca6721e8832ad9295ab07099192f
lilliputten <lilliputten@yandex.ru>
2020-02-23 01:58:24 +0300

    util-update-build-tag.py

--- util-update-build-tag.py
+++ util-update-build-tag.py
@@ -1,9 +1,26 @@
 # -*- coding:utf-8 -*-
 # @module util-update-build-tag
 # @since 2020.02.23, 00:58
-# @changed 2020.02.23, 00:58
+# @changed 2020.02.23, 01:58
 
+import json
 import datetime
-mylist = []
-today = datetime.date.today()
-print("OK")
+
+buildTagFile = 'build-tag.txt'
+
+dateTagFormat = '%y%m%d-%H%M'
+shortDateFormat = '%Y.%m.%d-%H:%M'
+detailedDateFormat = shortDateFormat + ':%S.%f'
+
+now = datetime.datetime.now()
+dateTag = now.strftime(dateTagFormat)
+
+pkgConfig = json.load(open('package.json'))
+version = pkgConfig['version']
+
+buildTag = 'v.' + version + '-' + dateTag
+
+print buildTag
+
+with open(buildTagFile, 'w') as f:
+    f.write(buildTag)
