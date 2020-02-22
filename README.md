6f229cce7583a324a1f8fae4949250cfee6351e3
lilliputten <lilliputten@yandex.ru>
2020-02-23 00:54:44 +0300

    Project env

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
  @changed 2020.02.23, 00:51
 -->
 
+See `.projectroot` file
+
 ## Commands
 ```shell
 grep CRON /var/log/syslog
--- util-update-build-tag.py
+++ util-update-build-tag.py
@@ -1,2 +1,9 @@
+# -*- coding:utf-8 -*-
+# @module util-update-build-tag
+# @since 2020.02.23, 00:58
+# @changed 2020.02.23, 00:58
 
+import datetime
+mylist = []
+today = datetime.date.today()
 print("OK")
